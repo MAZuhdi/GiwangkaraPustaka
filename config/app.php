3b41c4389409278e6ced85043ba67669467ec5e5
Muhammad Agung Zuhdi <janissaryagung@gmail.com>
2022-09-05 20:54:48 +0700

    app name and welcome page

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Giwangkara Pustaka'),
+    'name' => 'Giwangkara Pustaka',
 
     /*
     |--------------------------------------------------------------------------
